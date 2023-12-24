30ccd863779cae438d4b44b45e42dce26a02d9e9
Mandip Chhetri <108076760+Mandip47@users.noreply.github.com>
2023-12-24 22:59:35 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Prompt the user for their name
+var userName = prompt("What is your name?");
+
+// Display a greeting with the user's name
+alert("Hello, " + userName + "! Welcome to the world of JavaScript!");
